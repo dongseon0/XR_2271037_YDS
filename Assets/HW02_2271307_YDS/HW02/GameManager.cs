50e993cfa4285258ee4e853ac3466192301fa6c0
Dongseon Yang <ydsperfect08@ewhain.net>
2025-03-30 00:44:20 +0900

    HW2_GameManager 생성

---
+++ Assets/HW02_2271307_YDS/HW02/GameManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public bool isRadioOn = false;
+    public bool isTVOn = false;
+
+    public bool isBoardOn = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 변경돼도 유지
+        }
+        else
+        {
+            Destroy(gameObject); // 씬을 이동할 때 GameManager가 중복 생성되지 않도록 방지
+        }
+    }
+
+    public void SaveState(string objName, bool state)
+    {
+        switch (objName)
+        {
+            case "Radio":
+                isRadioOn = state;
+                break;
+            case "monitor":
+                isTVOn = state;
+                break;
+            case "DisplayBoard":
+                isBoardOn = state;
+                break;
+        }
+    }
+
+    public bool LoadState(string objName)
+    {
+        return objName switch
+        {
+            "Radio" => isRadioOn,
+            "monitor" => isTVOn,
+            "DisplayBoard" => isBoardOn,
+            _ => false
+        };
+    }
+}
