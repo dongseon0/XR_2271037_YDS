f0f9e46d6e9819bafc163f79dc10178e852cb6f4
Dongseon Yang <ydsperfect08@ewhain.net>
2025-04-01 09:07:05 +0900

    D09_Animation Controller 완성

--- Assets/D09_Animation/D09_Anim_Controller.cs
+++ Assets/D09_Animation/D09_Anim_Controller.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class D09_Anim_Controller : MonoBehaviour
 {
     public Animator animator;
+    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +20,15 @@ public class D09_Anim_Controller : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.P)){
             animator.speed = 1f;        // 공 재생
+            slider.value = 1f;
         }
         if(Input.GetKeyDown(KeyCode.O)){
             animator.speed = 0f;        // 공 정지
+            slider.value = 0f;
         }
+    }
+
+    public void OnSlider_SetSpeed(){
+        animator.speed = slider.value; // 공의 속도 = 슬라이더의 속도
     }
 }
