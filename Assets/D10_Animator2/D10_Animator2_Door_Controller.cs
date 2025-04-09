3334f799722dcb62919e9b0f991f06a7e37ecf0e
Dongseon Yang <ydsperfect08@ewhain.net>
2025-04-09 12:43:11 +0900

    Update D10_Animator2_Door_Controller.cs

--- Assets/D10_Animator2/D10_Animator2_Door_Controller.cs
+++ Assets/D10_Animator2/D10_Animator2_Door_Controller.cs
@@ -9,7 +9,7 @@ public class D10_Animator2_Door_Controller : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        animator.speed = 1;
+        animator.speed = 1; //다시 재생
     }
 
     private void OnTriggerExit(Collider other)
