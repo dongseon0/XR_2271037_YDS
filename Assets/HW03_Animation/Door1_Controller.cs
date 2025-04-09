0fe1bb56af9a4418cb70bc4205160b26ed308d46
Dongseon Yang <ydsperfect08@ewhain.net>
2025-04-09 13:46:55 +0900

    HW3_Door animator script 수정
    더 수정 필요..

---
+++ Assets/HW03_Animation/Door1_Controller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door1_Controller : MonoBehaviour
+{
+    public Animator animator;
+    public bool isPlayerInside = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        isPlayerInside = true;
+        
+        Vector3 toPlayer = (other.transform.position - transform.position).normalized; //문에서 플레이어를 향한 방향 벡터 계산
+        Vector3 doorForward = transform.forward;
+        float dot = Vector3.Dot(doorForward, toPlayer);
+
+        if (dot < 0) //내적 계산으로 방향 확인
+        {
+            animator.SetTrigger("OpenForward");
+        }
+        else
+        {
+            animator.SetTrigger("OpenBackward");
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        if (!other.CompareTag("Player")) return;
+
+        isPlayerInside = false;
+        animator.SetTrigger("Close");
+    }
+}
