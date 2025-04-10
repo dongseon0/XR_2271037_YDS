1ab1cba89cfd32d693732abb99b6491b159f3d9a
Dongseon Yang <ydsperfect08@ewhain.net>
2025-04-10 09:01:16 +0900

    HW3_Vehicle Collider & Animator 생성

---
+++ Assets/HW03_Animation/Ride_Controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ride_Controller : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Vehicle"){
+            transform.SetParent(other.transform);
+            transform.position = other.transform.position + Vector3.up*2f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        
+    if(other.name == "Vehicle"){
+        transform.SetParent(null);
+    }
+        
+    }
+}
